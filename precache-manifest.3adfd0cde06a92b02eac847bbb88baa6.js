f1666edcca98d93b0cbc4437cca594d54495b6a9
Dmitry <dzybaty@gmail.com>
2020-10-21 01:54:24 +0300

    Updates

---
+++ precache-manifest.3adfd0cde06a92b02eac847bbb88baa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9aabfcad72317249354478a230eb53c",
+    "url": "/phasmopicker/index.html"
+  },
+  {
+    "revision": "5111a4583c357b064678",
+    "url": "/phasmopicker/static/css/main.ef09de34.chunk.css"
+  },
+  {
+    "revision": "3cbf67764d1fb1d30cd4",
+    "url": "/phasmopicker/static/js/2.dce35546.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "/phasmopicker/static/js/2.dce35546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5111a4583c357b064678",
+    "url": "/phasmopicker/static/js/main.96b656a5.chunk.js"
+  },
+  {
+    "revision": "cc1a6d4811899b655f89",
+    "url": "/phasmopicker/static/js/runtime-main.b9d4abce.js"
+  }
+]);
