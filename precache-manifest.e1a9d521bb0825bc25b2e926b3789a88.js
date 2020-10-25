d2a0c03bca8a291335a0be81263034a656c930c0
Dmitry <dzybaty@gmail.com>
2020-10-25 09:08:05 +0300

    Updates

---
+++ precache-manifest.e1a9d521bb0825bc25b2e926b3789a88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7ec178ed7f87e21aef9512f9d7a607",
+    "url": "/phasmopicker/index.html"
+  },
+  {
+    "revision": "df787705d9cc3fa36a0d",
+    "url": "/phasmopicker/static/css/main.c1492104.chunk.css"
+  },
+  {
+    "revision": "dbc779b3fc95a27baa51",
+    "url": "/phasmopicker/static/js/2.53cdaa09.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "/phasmopicker/static/js/2.53cdaa09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df787705d9cc3fa36a0d",
+    "url": "/phasmopicker/static/js/main.d72773da.chunk.js"
+  },
+  {
+    "revision": "cc1a6d4811899b655f89",
+    "url": "/phasmopicker/static/js/runtime-main.b9d4abce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phasmopicker/precache-manifest.860e131031f2e9c62b144bdbc281e77c.js"
+  "/phasmopicker/precache-manifest.e1a9d521bb0825bc25b2e926b3789a88.js"
 );
 
 self.addEventListener('message', (event) => {
