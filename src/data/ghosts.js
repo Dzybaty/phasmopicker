88761764fdfe05c0ca87e1b2c034e1d0decdba06
Dmitry Sitsko <dzybaty@gmail.com>
2021-05-24 03:04:12 +0300

    Add new ghosts

--- src/data/ghosts.js
+++ src/data/ghosts.js
@@ -7,85 +7,99 @@ const ghosts = [
   {
     name: 'Banshee',
     evidences: [EMF, FINGERPRINTS, TEMPERATURE],
-    strength: 'Only targets one player at a time',
-    weakness: 'Hates the Crucifix',
+    strength: 'Only targets one player at a time.',
+    weakness: 'Hates the Crucifix.',
   },
 
   {
     name: 'Demon',
     evidences: [TEMPERATURE, GHOST_WRITING, SPIRIT_BOX],
-    strength: 'Extremely aggressive',
-    weakness: 'Using the Ouija board to ask questions does not drain sanity',
+    strength: 'Extremely aggressive.',
+    weakness: 'Using right Ouija board questions does not drain sanity.',
   },
 
   {
     name: 'Jinn',
     evidences: [EMF, SPIRIT_BOX, GHOST_ORB],
-    strength: 'Moves faster the further away you are from it',
-    weakness: 'Cutting off the location’s power supply will limit the Jinn’s speed',
+    strength: 'Moves faster the further away you are from it.',
+    weakness: 'Cutting off the location’s power supply will limit the Jinn’s speed.',
   },
 
   {
     name: 'Mare',
     evidences: [TEMPERATURE, SPIRIT_BOX, GHOST_ORB],
-    strength: 'Attacks more frequently in the dark, tends to switch off lights',
-    weakness: 'Keeping the lights on',
+    strength: 'Attacks more frequently in the dark, tends to switch off lights.',
+    weakness: 'More passive when lights are on.',
   },
 
   {
     name: 'Oni',
     evidences: [EMF, GHOST_WRITING, SPIRIT_BOX],
-    strength: 'Extremely active and moves objects quickly',
-    weakness: 'Extreme activity with lots of players nearby makes it easy to identify',
+    strength: 'Extremely active and moves objects quickly.',
+    weakness: 'Extreme activity with lots of players nearby makes it easy to identify.',
   },
 
   {
     name: 'Phantom',
     evidences: [EMF, TEMPERATURE, GHOST_ORB],
-    strength: 'Looking at the Phantom will reduce your sanity',
-    weakness: 'Capturing a photo will cause it to disappear, not during a hunt',
+    strength: 'Looking at the Phantom will reduce your sanity.',
+    weakness: 'Capturing a photo will cause it to disappear, not during a hunt.',
   },
 
   {
     name: 'Poltergeist',
     evidences: [FINGERPRINTS, SPIRIT_BOX, GHOST_ORB],
-    strength: 'Throws multiple objects about at once',
-    weakness: 'Rooms without any stuff in to throw',
+    strength: 'Throws multiple objects about at once.',
+    weakness: 'Rooms without any stuff in to throw.',
   },
 
   {
     name: 'Revenant',
     evidences: [EMF, FINGERPRINTS, GHOST_WRITING],
-    strength: 'Attacks anyone, regardless of sanity levels',
-    weakness: 'Moves extremely slowly when players hide',
+    strength: 'Attacks anyone, regardless of sanity levels.',
+    weakness: 'Moves extremely slowly when players hide.',
   },
 
   {
     name: 'Shade',
     evidences: [EMF, GHOST_WRITING, GHOST_ORB],
-    strength: 'Minimal activity, hard to find and detect',
-    weakness: 'Does not hunt if players are in a group',
+    strength: 'Minimal activity, hard to find and detect.',
+    weakness: 'Does not hunt if players are in a group.',
   },
 
   {
     name: 'Spirit',
     evidences: [FINGERPRINTS, GHOST_WRITING, SPIRIT_BOX],
-    strength: 'No strengths',
-    weakness: 'Using smudge sticks stops from starting for a long time',
+    strength: 'No strengths.',
+    weakness: 'Using smudge sticks prevent hunting for a long time.',
   },
 
   {
     name: 'Wraith',
     evidences: [FINGERPRINTS, TEMPERATURE, SPIRIT_BOX],
-    strength: 'Can fly through walls and does not leave footprints',
-    weakness: 'Salt is toxic for Wraith',
+    strength: 'Almost never touches the ground so it can\'t be tracked by footsteps.',
+    weakness: 'Salt is toxic for Wraith.',
   },
 
   {
     name: 'Yurei',
     evidences: [TEMPERATURE, GHOST_WRITING, GHOST_ORB],
-    strength: 'Drains player sanity especially quickly',
-    weakness: 'Smudge stick in the same room will stop the Yurei from moving',
+    strength: 'Drains player sanity especially quickly.',
+    weakness: 'Smudge stick in the same room will stop the Yurei from moving.',
+  },
+
+  {
+    name: 'Yokai',
+    evidences: [SPIRIT_BOX, GHOST_ORB, GHOST_WRITING],
+    strength: 'Talking near a Yokai will anger it and increase it\'s chance of attacking.',
+    weakness: 'When hunting a Yokai can only hear voices close to it.',
+  },
+
+  {
+    name: 'Hantu',
+    evidences: [FINGERPRINTS, GHOST_ORB, GHOST_WRITING],
+    strength: 'Moves faster in cooler areas.',
+    weakness: 'Moves slower in warmer areas.',
   },
 ];
 
