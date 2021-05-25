c94bea61210900d3e8031fb85f71aa02a2cb3c4c
Dmitry Sitsko <dzybaty@gmail.com>
2021-05-25 18:52:08 +0300

    Mark new ghosts as beta

--- src/data/ghosts.js
+++ src/data/ghosts.js
@@ -89,14 +89,14 @@ const ghosts = [
   },
 
   {
-    name: 'Yokai',
+    name: 'Yokai [BETA]',
     evidences: [SPIRIT_BOX, GHOST_ORB, GHOST_WRITING],
     strength: 'Talking near a Yokai will anger it and increase it\'s chance of attacking.',
     weakness: 'When hunting a Yokai can only hear voices close to it.',
   },
 
   {
-    name: 'Hantu',
+    name: 'Hantu [BETA]',
     evidences: [FINGERPRINTS, GHOST_ORB, GHOST_WRITING],
     strength: 'Moves faster in cooler areas.',
     weakness: 'Moves slower in warmer areas.',
