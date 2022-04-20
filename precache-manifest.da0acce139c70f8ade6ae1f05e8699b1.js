866ccf0c4552a879dd6e7a01c259382cb2bfb74e
Dmitry Sitsko <dzybaty@gmail.com>
2022-04-20 16:04:19 +0300

    Updates

--- precache-manifest.da0acce139c70f8ade6ae1f05e8699b1.js
+++ precache-manifest.da0acce139c70f8ade6ae1f05e8699b1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8e84c20e087f19140b43da590200e1c6",
+    "revision": "ca95f76c7dbe0fef59786c5d5a6ee6a5",
     "url": "/phasmopicker/index.html"
   },
   {
-    "revision": "c0b6ef86e5e231906f7e",
+    "revision": "485c37ebd4f6f52e38eb",
     "url": "/phasmopicker/static/css/main.28cc7bdf.chunk.css"
   },
   {
-    "revision": "18eefeac586d6d1cca16",
-    "url": "/phasmopicker/static/js/2.eaad1ad3.chunk.js"
+    "revision": "2336065917b5762fb786",
+    "url": "/phasmopicker/static/js/2.49b19049.chunk.js"
   },
   {
     "revision": "1770c0dd558b5b9f7b2183c33890f7ca",
-    "url": "/phasmopicker/static/js/2.eaad1ad3.chunk.js.LICENSE.txt"
+    "url": "/phasmopicker/static/js/2.49b19049.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0b6ef86e5e231906f7e",
-    "url": "/phasmopicker/static/js/main.66d6c2e2.chunk.js"
+    "revision": "485c37ebd4f6f52e38eb",
+    "url": "/phasmopicker/static/js/main.4447c8c5.chunk.js"
   },
   {
     "revision": "cc1a6d4811899b655f89",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phasmopicker/precache-manifest.6e2c226095906c3548ed97b8e5332cd2.js"
+  "/phasmopicker/precache-manifest.da0acce139c70f8ade6ae1f05e8699b1.js"
 );
 
 self.addEventListener('message', (event) => {
